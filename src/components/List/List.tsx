2c8633152dfc71850eb8f9684765f18b8f484aa2
Amit <amit@Amits-MBP.alphionsee.in>
2023-09-17 01:33:12 +0530

    imporved ui of cart page

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,27 +3,34 @@ import { ProductType } from "../../types/types";
 interface ListPropsType {
   productInfo: ProductType;
   cnt: number;
+  onDelete: (id: number) => void;
 }
-export const List: React.FC<ListPropsType> = ({ productInfo, cnt }) => {
+export const List: React.FC<ListPropsType> = ({
+  productInfo,
+  cnt,
+  onDelete,
+}) => {
   return (
-    <div className="flex items-center gap-5 bg-slate-200 p-[10px] justify-between ">
-      <div>
-        <img
-          src={productInfo.imageURL}
-          className="max-w-[100px] w-full rounded-md"
-        />
-      </div>
-      <div>
+    <div className="bg-slate-200 shadow-lg p-[10px]  ">
+      <div className="flex gap-2 text-sm mb-2">
         <p>{productInfo.name}</p>
         <p>{`${productInfo.currency} ${productInfo.price}`}</p>
       </div>
-      <div className="flex items-center gap-5 ">
-        <button className="bg-white   text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-          Qnt:{cnt}
-        </button>
-        <button className="bg-rose-500 text-white font-bold py-2 px-4 rounded">
-          Delete
-        </button>
+      <div className="flex rounded-md items-center gap-5  justify-between ">
+        <div className="p-[5px]">
+          <img src={productInfo.imageURL} className=" h-[100px] rounded-md" />
+        </div>
+        <div className="flex items-center gap-5 ">
+          <button className="bg-white   text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow pointer-events-none">
+            Qnt:{cnt}
+          </button>
+          <button
+            onClick={() => onDelete(productInfo.id)}
+            className="bg-rose-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/routes/Cart/Cart.tsx
+++ src/routes/Cart/Cart.tsx
@@ -2,17 +2,27 @@ import { List } from "../../components/List/List";
 import useProduct from "../../hooks/useProduct";
 
 export const Cart = () => {
-  const { cartItems, productsInfo } = useProduct();
+  const { cartItems, productsInfo, deleteFromCart } = useProduct();
   const selectedProducts = productsInfo.filter((product) => {
     return cartItems[product.id];
   });
 
+  function handleDelete(id: number) {
+    deleteFromCart(id);
+  }
+
   return (
-    <div className="p-[20px]">
+    <div className="md:p-[20px] p-[10px]">
       <div>Shoopping Cart</div>
-      <div className="flex flex-col gap-5 max-w-md mx-auto">
+      <div className="flex flex-col gap-5 max-w-md mx-auto h-[calc(100vh-120px)] overflow-scroll scroll-smooth ">
         {selectedProducts.map((item) => {
-          return <List productInfo={item} cnt={cartItems[item.id]} />;
+          return (
+            <List
+              productInfo={item}
+              cnt={cartItems[item.id]}
+              onDelete={handleDelete}
+            />
+          );
         })}
       </div>
     </div>
