56ea99a33522ffd6cbe3c8e4ee8aab54ff29c9af
Amit <amit@Amits-MBP.alphionsee.in>
2023-09-17 15:34:43 +0530

    updated folder name

--- src/components/Filter/Filters.tsx
+++ src/components/Filter/Filters.tsx
@@ -1,5 +1,5 @@
 import { FilterType } from "../../types/types";
-import CheckBox from "../checkBox/CheckBox";
+import CheckBox from "../checkbox/CheckBox";
 import { FiltersData } from "./FilterConfig";
 
 interface FiltersPropType {
